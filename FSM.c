045653d7d13f9a149d1b4466efe07898deb42cf6
Martell94 <116161025+Martell94@users.noreply.github.com>
2022-11-02 09:42:24 +0100

    Update FSM.c

--- FSM.c
+++ FSM.c
@@ -1,12 +1,56 @@
 #include "header.h"
 
-void fsm_reset (void){
+static door_t door_state = DOOR_STATE_CLOSED; //Dörr stängd vid start
+
+void fsm_reset (void){ //Defaultar till closed
 	
+	door_state = DOOR_STATE_CLOSED;
+	LED1_OFF;
+	LED2_ON;
+	LED3_OFF;
 	return;
 }
 
-void fsm_update (void){
+void fsm_update (void){ //Uppdaterar tillståndet baserat på door_state-variabeln
 	
+	switch (door_state){
+		
+		case DOOR_STATE_OPEN:{
+			
+			if (CLOSE_B_PRESSED){
+				door_state = DOOR_STATE_CLOSED;
+			} 
+			else if (LOCK_B_PRESSED){
+				door_state = DOOR_STATE_ERROR;
+			}
+			break;			
+		}
+		
+		case DOOR_STATE_CLOSED:{
+			
+			if (OPEN_B_PRESSED){
+				door_state = DOOR_STATE_OPEN;
+			}
+			else if (LOCK_B_PRESSED){
+				door_state = DOOR_STATE_LOCKED;
+			}
+			break;
+		}
+		
+		case DOOR_STATE_LOCKED:{
+			
+			if (CLOSE_B_PRESSED){
+				door_state = DOOR_STATE_CLOSED;
+			}
+			else if (OPEN_B_PRESSED){
+				door_state = DOOR_STATE_ERROR;
+			}
+			break;
+		}
+		case DOOR_STATE_ERROR:{
+			fsm_update();
+		}
+	}
 	return;
 }
 
